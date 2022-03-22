d3877dfd2299496856b954ce39ef898ea28b260e
Mintimer <mintimer99@gmail.com>
2022-03-22 12:30:32 +0300

    added and set up initially tableView and tableViewCell

--- DictionaryApp/ViewController.swift
+++ DictionaryApp/ViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var languageButton: UIBarButtonItem!
+    @IBOutlet weak var dictionaryTableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    
+    @IBAction func switchLanguageButton(_ sender: UIBarButtonItem) {
+    }
+    
 
 
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? WordTVCell else {return UITableViewCell()}
+        return cell
+    }
+    
+    
+    
+    
 }
 
---
+++ DictionaryApp/WordTVCell.swift
@@ -0,0 +1,26 @@
+//
+//  WordTVCell.swift
+//  DictionaryApp
+//
+//  Created by Минтимер Харасов on 22.03.2022.
+//
+
+import UIKit
+
+class WordTVCell: UITableViewCell {
+
+    
+    @IBOutlet weak var wordLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
